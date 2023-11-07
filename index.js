32ae8096fdf180a077cda30e932e9c39c7a7eed3
rafael17cordeiro <59150464+rafael17cordeiro@users.noreply.github.com>
2023-11-07 23:32:45 +0000

    ok

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function login() {
     localStorage.setItem("idade", idade);
     localStorage.setItem("gmail", gmail);
     localStorage.setItem("password", password);
-    
+    localStorage.setItem("points", 0);
     document.getElementById("login-bg").style= "box-shadow: 10px 10px 10px 0px rgb(0, 152, 56);"
     document.getElementById("alert").textContent=""
     setTimeout(function() {
@@ -88,4 +88,61 @@ function reset() {
     fisrtName.value="";
     lastName.value="";
     message.value="";
+}
+
+
+
+
+
+/*games*/
+
+const randomNumber = Math.floor(Math.random() * 5) + 1;
+let attempts = 0;
+
+
+let points = parseInt(localStorage.getItem("points"));
+
+
+
+
+
+document.getElementById("pointsDisplay").textContent = "Points: " + points + "";
+
+function resetPoints() {
+    points = 0;
+    localStorage.setItem("points", points);
+    document.getElementById("pointsDisplay").textContent = "Points: " + points + "";
+}
+
+
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById("guess").value);
+
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > 5) {
+        document.getElementById("message").textContent = "* Please enter a valid number between 1 and 5.";
+        document.getElementById("message").style.color = "tomato";
+        document.getElementById("game1").style.boxShadow = "10px 10px 10px 0px red";
+        return;
+    }
+
+    attempts++;
+
+    if (userGuess === randomNumber) {
+        document.getElementById("message").textContent = `Congratulations! You guessed the number ${randomNumber} in ${attempts} attempts. +500 Points!`;
+        document.getElementById("message").style.color = "lightgreen";
+        document.getElementById("game1").style.boxShadow = "10px 10px 10px 0px rgb(0, 152, 56)";
+        points += 500;
+        console.log(points);
+
+     
+        localStorage.setItem("points", points);
+
+        setTimeout(function() {
+            location.href = "../games.html";
+        }, 1500);
+    } else {
+        document.getElementById("message").style.color = "white";
+        document.getElementById("game1").style.boxShadow = "10px 10px 10px 0px rgb(0, 125, 209)";
+        document.getElementById("message").textContent = "Try another number!";
+    }
 }
